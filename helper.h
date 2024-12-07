cdf62fd1d4a0eeeb4abb6b08c5bd27d7ad779cf5
Alan044 <rich@Alan.>
2024-12-07 20:08:26 +0100

    Nu är allt fullt refaktorerat i två moduler, helper som bara har hjälpfunktioner medan util har funktionerna som Main använder sig av

--- helper.h
+++ helper.h
@@ -7,4 +7,17 @@ bool valid_length_and_char(char* buf, char* error);
 
 bool valid_year(char* buf, char* error);
 
+char* ask_date();
+
+void resize_array(tasks* arr);
+
+task_t* create_task(char* name, char* date, tasks* arr);
+
+option_t valid_name(tasks* arr, char *name_to_remove);
+
+int calculate_days(task_t* task);
+
+
+
+
 #endif 
--- util.c
+++ util.c
@@ -24,44 +24,6 @@ input_t valid_input(char* input)
     input_t input2 = { .input = "p", .valid = false};
     return input2;
 }
-  
-char* ask_date() 
-{
-    char buf[256];
-    while (true) {
-    printf("When is the task due? \n");
-    printf("Please write the following date on this format xxxx-xx-xx \n");
-    scanf("%255s", buf);
-    char* error = "Invalid date";
-    for (int i = 0; i < 10; i++ ) {
-
-
-        if (!(valid_year(buf, error))) {
-            printf("%s \n", error);
-            break;
-        }
-        if(!(valid_length_and_char(buf, error))) {
-            break;
-        }
-        
-            if(isdigit(buf[i])) {
-                return strdup(buf);
-            } else {
-            printf("%s \n", error);
-            break;
-            }
-        }
-    }    
-}
-
-char* ask_task()
-{
-    char buf[100];
-    printf("What should the new name be: ?\n");
-    scanf(" %99s", buf);
-    return strdup(buf);
-}
-
 
 tasks* create_array() 
 {
@@ -70,37 +32,6 @@ tasks* create_array()
     arr->array_of_tasks = calloc(arr->capacity, sizeof(task_t*));
     arr->size = 0;
     return arr;
-}
-
-void resize_array(tasks* arr) {
-    if (arr->size >= arr->capacity) {
-        task_t** new_array = realloc(arr->array_of_tasks, arr->capacity * 2 * sizeof(task_t*));
-        if (!new_array) {
-            perror("Failed to resize array");
-            exit(EXIT_FAILURE);
-        }
-        arr->array_of_tasks = new_array;
-        arr->capacity *= 2;
-    }
-}
-
-task_t* create_task(char* name, char* date, tasks* arr) 
-{
-    resize_array(arr);  
-
-    task_t* new_task = calloc(1, sizeof(task_t));
-    info_t* info_i = calloc(1, sizeof(info_t));
-
-    new_task->name = name;
-    new_task->date = date;
-    new_task->info = info_i;
-    new_task->info->index = arr->size;
-    int total_days = calculate_days(new_task);
-    new_task->info->days_left = total_days;
-
-    arr->array_of_tasks[arr->size] = new_task;
-    arr->size += 1;
-    return new_task;
 }
 
 task_t* create_task_info(tasks* arr) 
@@ -133,21 +64,13 @@ void destroy(tasks* arr) {
     free(arr->array_of_tasks);
     free(arr);
 }
-
-option_t valid_name(tasks* arr, char *name_to_remove)
+char* ask_task()
 {
-    for (int i = 0; i < arr->size; i++) 
-    {
-        if (strcmp(arr->array_of_tasks[i]->name,name_to_remove) == 0) {
-            option_t option = {.valid = true, .index = i};
-            return option;
-        }
-    
-    }
-    option_t option = {.valid = false, .index = arr->capacity + 1};
-    return option;
+    char buf[100];
+    printf("What should the new name be: ?\n");
+    scanf(" %99s", buf);
+    return strdup(buf);
 }
-
 
 void remove_task(tasks*arr, char*name_to_remove)
 {
@@ -195,30 +118,3 @@ void edit_task(tasks* arr, char* name_to_edit)
     printf("Could not find the name ");
 }
 
-int calculate_days(task_t* task)
-{
-    time_t now = time(NULL);
-    struct tm *current = localtime(&now);
-    
-    date_t date = task_date(task->date);
-    struct tm *task_date = localtime(&now);
-
-    int year_int = atoi(date.year);
-    int month_int = atoi(date.month) -1;
-    if (date.day[0] == '0') {
-        int day_int = atoi(&date.day[1]);
-        task_date->tm_mday = day_int; 
-    } else {
-        int day_int = atoi(date.day);    
-        task_date->tm_mday = day_int; 
-    }
-    task_date->tm_year = year_int -1900; 
-    task_date->tm_mon = month_int; 
-
-    int seconds = difftime(mktime(task_date) ,  now);
-    int days = seconds / (60* 60 * 24);
-    free(date.year);
-    free(date.month);
-    free(date.day);   
-    return days;
-}
